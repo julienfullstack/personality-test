17e49542409f1c2dba77da7c3a609a738041b719
Julien L <julienlenaz96@gmail.com>
2023-06-16 20:54:11 -0400

    Continued debugging to allow results to display

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,36 @@ function hideResultsAndError() {
   document.getElementById("monkey").setAttribute("class", "hidden");
   document.getElementById("owl").setAttribute("class", "hidden");
   document.getElementById("dog").setAttribute("class", "hidden");
+  document.getElementById("dog").setAttribute("class", "hidden");
 } 
-
-
 
 window.addEventListener("load", function() {
   let form = document.querySelector("form");
   let resetBtn = document.getElementById("reset");
+  let colorVal = parseInt(document.getElementById("color").value);
+  let beverageVal = parseInt(document.getElementById("beverage").value);
+  let locationVal = parseInt(document.getElementById("location").value);
+  let hobbyVal = parseInt(document.getElementById("hobby")).value;
   hideResultsAndError();
+});
+
+  resetBtn.addEventListener("click", function() {
+    document.getElementById("color").value = null;
+    document.getElementById("beverage").value = null;
+    document.getElementById("location").value = null;
+    document.getElementById("hobby").value = null;
+  });
+  
 
   form.addEventListener("submit", function(event) {
-    event.preventDefault();
+    resetBtn.removeAttribute("class");
+  });
 
-    const colorVal = parseInt(document.getElementById("color").value);
-    const beverageVal = parseInt(document.getElementById("beverage").value);
-    const locationVal = parseInt(document.getElementById("location").value);
-    const hobbyVal = parseInt(document.getElementById("hobby")).value;
-    ans= colorVal + beverageVal + locationVal + hobbyVal;
-
+  let ans= colorVal + beverageVal + locationVal + hobbyVal;
     if( ans >= 0){
       document.getElementById("cat").removeAttribute("class");
       document.getElementById("elephant").removeAttribute("class");
       document.getElementById("owl").removeAttribute("class");
       document.getElementById("monkey").removeAttribute("class");
       document.getElementById("dog").removeAttribute("class");
-  )}
+    };
